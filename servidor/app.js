20144da0d00d12d2c21d3de4bad0a3b055b4f320
Enrique <enrique.barra@upm.es>
2022-05-20 10:10:27 +0200

    start command

--- servidor/app.js
+++ servidor/app.js
@@ -5,7 +5,18 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/index.html");
+});
+
 app.get("/getData", (req, res, next) => {
+  //1. coger los params de req.params
+
+
+  //2. Llamadas a apis (Google, HIBP, Twitter, ...)
+
+
+  //3. Devolver los datos
   res.json({clave: "valor"});
  });
 
